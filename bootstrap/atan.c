68bb7be2deb3f62cfd652527d10185c3c4001270
FanoPlanes <pontryagin@protonmail.com>
2026-05-17 19:35:48 +0200

    Style.

--- bootstrap/atan.c
+++ bootstrap/atan.c
@@ -1,39 +1,44 @@
-double power(double n, int m) {
-    double i = 1;
-    for (int j = 0; j < m; j++) {
-        i *= n;
-    }
-    return i;
+double power(double n, int m)
+{
+	double i = 1;
+	for (int j = 0; j < m; j++) {
+		i *= n;
+	}
+	return i;
 }
 
-double factorial(int n) {
-    if (n == 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+double factorial(int n)
+{
+	if (n == 0) {
+		return 1;
+	} else {
+		return n * factorial(n - 1);
+	}
 }
 
-double product_term(double x, int k) {
-    return (2. * k / (2. * k + 1.)) * (x * x / (1 + x * x));
+double product_term(double x, int k)
+{
+	return (2. * k / (2. * k + 1.)) * (x * x / (1 + x * x));
 }
 
-double series_term(double x, int n) {
-    double result = 1;
-    for (int i = 1; i <= n; i++) {
-        result *= product_term(x, i);
-    }
-    return result;
+double series_term(double x, int n)
+{
+	double result = 1;
+	for (int i = 1; i <= n; i++) {
+		result *= product_term(x, i);
+	}
+	return result;
 }
 
-double my_atan(double x) {
-    double res = 0;
-    double res_prev = 1;
-    int i = 0;
-    while (res_prev != res) {
-        res_prev = res;
-        res += series_term(x, i);
-        i++;
-    }
-    return res * x / (1 + x * x);
+double my_atan(double x)
+{
+	double res = 0;
+	double res_prev = 1;
+	int i = 0;
+	while (res_prev != res) {
+		res_prev = res;
+		res += series_term(x, i);
+		i++;
+	}
+	return res * x / (1 + x * x);
 }
--- bootstrap/pi.c
+++ bootstrap/pi.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 // calculate pi for use with CORDIC, get double precision
-int main(void) {
-    double pi = 16 * my_atan(1. / 5) - 4 * my_atan(1. / 239);
-    FILE *fp = fopen("pi.h", "w");
-    fprintf(fp, "//pi constant calculated in pi.c\n#define MY_PI %.14f", pi);
-    fclose(fp);
-    return 0;
+int main(void)
+{
+	double pi = 16 * my_atan(1. / 5) - 4 * my_atan(1. / 239);
+	FILE *fp = fopen("pi.h", "w");
+	fprintf(fp, "//pi constant calculated in pi.c\n#define MY_PI %.14f", pi);
+	fclose(fp);
+	return 0;
 }
