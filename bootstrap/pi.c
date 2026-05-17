ef1ac0af34ee449667fa53e591768c012ae0ab39
FanoPlanes <pontryagin@protonmail.com>
2026-05-17 18:07:56 +0200

    Cleanup, remove unneded definition of fixnum pi.

--- bootstrap/pi.c
+++ bootstrap/pi.c
@@ -4,10 +4,8 @@
 // calculate pi for use with CORDIC, get double precision
 int main(void) {
     double pi = 16 * my_atan(1. / 5) - 4 * my_atan(1. / 239);
-    long long ll_pi = pi * 1e14;
     FILE *fp = fopen("pi.h", "w");
-    fprintf(fp, "//pi constant calculated in pi.c\n#define MY_PI %.14f\n", pi);
-    fprintf(fp, "#define MY_FIXNUMPI %Li", ll_pi);
+    fprintf(fp, "//pi constant calculated in pi.c\n#define MY_PI %.14f", pi);
     fclose(fp);
     return 0;
 }
--- pi.h
+++
@@ -1,3 +0,0 @@
-//pi constant calculated in pi.c
-#define MY_PI 3.14159265358979
-#define MY_FIXNUMPI 314159265358979
